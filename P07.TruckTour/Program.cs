3ca2bd5c400f3d9b78bb830ed56cb029a8968b4d
Dragomira <132193889+dragomirak@users.noreply.github.com>
2024-01-14 18:08:45 +0200

    Adding P07.TruckTour

---
+++ P07.TruckTour/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P07.TruckTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int petrolPumps = int.Parse(Console.ReadLine());
+            Queue<int[]> pumpsQueue = new Queue<int[]>();
+
+            for (int i = 0; i < petrolPumps; i++)
+            {
+                int[] data = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                pumpsQueue.Enqueue(data);
+            }
+
+            int position = 0;
+            while (true)
+            {
+                int fuel = 0;
+
+                foreach (var pump in pumpsQueue)
+                {
+                    fuel += pump[0] - pump[1];
+                    if (fuel < 0)
+                    {
+                        position++;
+                        pumpsQueue.Enqueue(pumpsQueue.Dequeue());
+                        break;
+                    }
+                }
+
+                if (fuel >= 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(position);
+        }
+    }
+}
